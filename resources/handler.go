b3b42061a6695f86917acdcfe401884a685fecc0
marina <marina.chompalova@ft.com>
2020-02-26 15:34:06 +0200

    Migrate to Go modules

--- resources/handler.go
+++ resources/handler.go
@@ -9,7 +9,7 @@ import (
 	"net/http"
 	"strings"
 
-	"github.com/Financial-Times/concept-rw-elasticsearch/service"
+	"github.com/Financial-Times/concept-rw-elasticsearch/v2/service"
 	log "github.com/Financial-Times/go-logger"
 	tid "github.com/Financial-Times/transactionid-utils-go"
 	"github.com/gorilla/mux"
